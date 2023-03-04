243e4adc07777a2e2bcb71d825d0738b17447503
Maks6831 <ma.khan6971@gmail.com>
2023-03-04 16:02:18 +0000

    completed Intern.js, All tests passed

--- starter/tests/Intern.test.js
+++ starter/tests/Intern.test.js
@@ -1,18 +1,18 @@
 const Intern = require("../lib/Intern");
 
-test("Can set school via constructor", () => {
+test("Can set school via constructor", () => { // done;
   const testValue = "UCLA";
   const e = new Intern("Foo", 1, "test@test.com", testValue);
   expect(e.school).toBe(testValue);
 });
 
-test("getRole() should return \"Intern\"", () => {
+test("getRole() should return \"Intern\"", () => { // done;
   const testValue = "Intern";
   const e = new Intern("Foo", 1, "test@test.com", "UCLA");
   expect(e.getRole()).toBe(testValue);
 });
 
-test("Can get school via getSchool()", () => {
+test("Can get school via getSchool()", () => { // done; 
   const testValue = "UCLA";
   const e = new Intern("Foo", 1, "test@test.com", testValue);
   expect(e.getSchool()).toBe(testValue);
