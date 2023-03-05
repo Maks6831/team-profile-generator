f7e0edff50684118a23724913e9649277d2e5672
Maks6831 <ma.khan6971@gmail.com>
2023-03-05 14:18:05 +0000

    added engineer and intern functions - program now fully functioning

--- starter/index.js
+++ starter/index.js
@@ -11,42 +11,73 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
+
 console.log('Sucess!!')
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+
+const enterName = {name: 'name', message: 'Enter Name'};
+const emailAdd = {name: "emailAdd", message: "Enter email address"};
+const employeeID = {name: 'employeeID', message: "Enter employee ID"};
+const officeNumber = {name: "officeNumb", message: "Enter office number"};
+const ID = {name: 'ID', message: 'Enter ID'};
+const githubUsername = {name: 'github', message: 'Enter your username'};
+const school = {name: 'school', message: 'Enter School'};
+const menu = {
+    type: 'list',
+    name: 'choice',
+    message: 'please choose an option',
+    choices: ['Add an Engineer', 'Add an intern', 'Finish building the team']
+}
+
+
+
+const createHtml = (team) => {
+    fs.writeFile(outputPath, render(team), (err) => err? console.log(err): console.log('success'));
+}
+
+const menuOption = (choice, team) => {
+    switch (choice) {
+        case 'Add an Engineer':
+            engineerPrompt(team);
+            break;
+        case 'Add an intern':
+            internPrompt(team);
+            break;
+        case 'Finish building the team':
+            console.log('switch statement is working');
+            createHtml(team);
+    }
+
+}
+
+const internPrompt = (team) => {
+    inquirer.prompt([enterName, ID, emailAdd, school, menu]).then((data)=>{
+        const intern = new Intern(data.name, data.ID, data.emailAdd, data.school);
+        team.push(intern);
+        menuOption(data.choice, team); 
+    })
+    
+}
+
+const engineerPrompt = (team) => {
+    inquirer.prompt([enterName, ID, emailAdd, githubUsername, menu]).then((data) => {
+        const engineer = new Engineer(data.name, data.ID, data.emailAdd, data.github, data.choice);
+        team.push(engineer);
+        menuOption(data.choice, team);
+    })
+}
+
 const startingPrompts = () => {
-    inquirer.prompt([{
-        name: 'tmName',
-        message: "Enter name"
-    },{
-        name: 'employeeID',
-        message: "Enter employee ID"
-    },{
-        name: "emailAdd",
-        message: "Enter email address"
-    },{
-        name: "officeNumb",
-        message: "Enter office number"
-    },{
-        type:'list',
-                name: 'choice',
-                message: 'Please choose an option',
-                choices: ['Add an Engineer', 'Add an intern', 'Finish building the team']
-    }]).then((data) => {
-        const manager = new Manager(data.tmName, data.employeeID, data.emailAdd, data.officeNum);
-        switch (data.choices) {
-            case 'Add an Engineer':
-                engineerPrompt();
-                break;
-            case 'Add an intern':
-                internPrompt();
-                break;
-            case 'Finish building the team':
-                render()
-        }
+    inquirer.prompt([enterName, employeeID, emailAdd, officeNumber, menu]).then((data) => {
+        const manager = new Manager(data.name, data.employeeID, data.emailAdd, data.officeNumb);
+        const team = [manager];
+        menuOption(data.choice, team);
         console.log(data);
+        console.log('this is the manager class object ---');
+        console.log(manager);
     })
 
     }
